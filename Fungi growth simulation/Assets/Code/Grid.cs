fed862982ad862982bd08d58a7cb3e18577dc193
Paweł <pawel.pycia29@gmail.com>
2022-05-17 15:38:43 +0200

    neighbors almost

--- Fungi growth simulation/Assets/Code/Grid.cs
+++ Fungi growth simulation/Assets/Code/Grid.cs
@@ -13,12 +13,32 @@ public class Grid
                 for (int z = 0; z < Config.GridSize; z++)
                     InitializeGridCell(x, y, z);
 
+
+
+        for (int x = 0; x < Config.GridSize; x++)      
+            for (int y = 0; y < Config.GridSize; y++)        
+                for (int z = 0; y < Config.GridSize; z++)
+                    SetNeighbors(x, y, z);            
+                
+            
+        
+
+
         for (int i = 0; i < Config.InitialChildrenCnt; i++) ;
     }
 
     private void InitializeGridCell(int x, int y, int z)
     {
         _gridCells[x, y, z] = new GridCell(x, y, z);
+    }
+
+    private void SetNeighbors(int x, int y, int z) 
+    {
+        _gridCells[x, y, z].
+        if (z % 2 == 0) 
+        {
+
+        }
     }
 
     public static bool IsPositionValid(int[] position)
--- Fungi growth simulation/Assets/Code/GridCell.cs
+++ Fungi growth simulation/Assets/Code/GridCell.cs
@@ -11,15 +11,15 @@ public class GridCell : MonoBehaviour
 
     public GridCell(int x, int y, int z)
     {
-        foreach (Direction direction in Enum.GetValues(typeof(Direction)))
-        {
-            Vector3 directionVector3 = DirectionMethods.ToVector3(direction);
-            int[] neighborPosition = { x + (int)directionVector3[0], 
-                                       y + (int)directionVector3[1], 
-                                       z + (int)directionVector3[2] };
-            if (Grid.IsPositionValid(neighborPosition))
-                _neighbors.Add(direction, )
-        }
+        // foreach (Direction direction in Enum.GetValues(typeof(Direction)))
+        // {
+        //     Vector3 directionVector3 = DirectionMethods.ToVector3(direction);
+        //     int[] neighborPosition = { x + (int)directionVector3[0], 
+        //                                y + (int)directionVector3[1], 
+        //                                z + (int)directionVector3[2] };
+        //     if (Grid.IsPositionValid(neighborPosition))
+        //         _neighbors.Add(direction, )
+        // }
     }
 
     // Start is called before the first frame update
