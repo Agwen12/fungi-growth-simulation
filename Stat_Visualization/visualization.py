549c2dffdc8a630026db611470b2a2a547b2d045
Pawel Pycia <pawel.pycia29@gmail.com>
2022-06-01 11:25:00 +0200

    added units

--- Stat_Visualization/visualization.py
+++ Stat_Visualization/visualization.py
@@ -11,25 +11,21 @@ Time,External Nutrition,Internal Nutrition,Active Hyphal,Tip
 """
 
 
-def create_plot(x_axis, data, y_label, path, title, plot_type="plot"):
+def create_plot(x_axis, data, y_label, path, title, y_unit=None, plot_type="plot"):
     y_axis = data
     fig, ax = plt.subplots(figsize=(12, 9))
-    a = getattr(ax, plot_type)
+    func = getattr(ax, plot_type)
     marker = "." if plot_type != "plot" else None
-    a(x_axis,
-            y_axis,
-            marker=marker,
-            c='orange',
-            linewidth=2)
+    func(x_axis, y_axis, marker=marker, c='orange', linewidth=2)
     ax.set_title(f"{title} Over Time")
-    ax.set_ylabel(y_label)
-    ax.set_xlabel("Time")
+    ax.set_ylabel(f"{y_label} ({y_unit})")
+    ax.set_xlabel("Time (days)")
     fig.savefig(os.path.join(path, title.replace(" ", "")))
 
 
 if __name__ == "__main__":
     data_file = sys.argv[1]
-    path = os.path.join(os.curdir, "data")
+    path = os.path.join(os.curdir)
     path = os.path.join(path, str(time.time()))
     os.mkdir(path)
     data = pd.read_csv(data_file)
@@ -38,13 +34,13 @@ if __name__ == "__main__":
 
     create_plot(x, data["Tip"], "Tip", path, "Tips Cells", "scatter")
     create_plot(x, data["Active Hyphal"], "Active Hyphal", path, "Active Cells")
-    create_plot(x, data["External Nutrition"], "External Nutrition", path, "External Nutrition")
-    create_plot(x, data["Internal Nutrition"], "Internal Nutrition", path, "Internal Nutrition")
-    create_plot(x, data["Internal Nutrition"]/hyphal_sum, "Average internal nutrition", path, "Average internal nutrition")
+    create_plot(x, data["External Nutrition"], "External Nutrition", path, "External Nutrition", "mol")
+    create_plot(x, data["Internal Nutrition"], "Internal Nutrition", path, "Internal Nutrition", "mol")
+    create_plot(x, data["Internal Nutrition"]/hyphal_sum, "Average internal nutrition", path,
+                "Average internal nutrition", "mol")
 
-    create_plot(x, hyphal_sum,
-                "Hyphal Cells", path, "Hyphal Cells")
+    create_plot(x, hyphal_sum, "Hyphal Cells", path, "Hyphal Cells")
     create_plot(x[:-1], [hyphal_sum[i+1] - hyphal_sum[i] for i in range(len(hyphal_sum) - 1)],
                 "Hyphal Difference", path, "Hyphal Difference", "scatter")
 
-    create_plot(x, hyphal_sum / data['Tip'], "HGU", path, "HGU")
+    create_plot(x, hyphal_sum / data['Tip'], "HGU", path, "HGU", "μm")
