977dc21283ff724131ef6e381dd9f7de9a53db53
Pawel Pycia <pawel.pycia29@gmail.com>
2022-06-02 13:10:53 +0200

    changed plotting

--- helper_scripts/Stat_Visualization/visualization.py
+++ helper_scripts/Stat_Visualization/visualization.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib
 import numpy as np
 import os
 import time
@@ -11,16 +12,16 @@ Time,External Nutrition,Internal Nutrition,Active Hyphal,Tip
 """
 
 
-def create_plot(x_axis, data, y_label, path, title, y_unit=None, plot_type="plot"):
+def create_plot(x_axis, data, y_label, title, ax, y_unit="", plot_type="plot"):
     y_axis = data
-    fig, ax = plt.subplots(figsize=(12, 9))
+    # fig, ax = plt.subplots(figsize=(12, 9))
     func = getattr(ax, plot_type)
     marker = "." if plot_type != "plot" else None
     func(x_axis, y_axis, marker=marker, c='orange', linewidth=2)
     ax.set_title(f"{title} Over Time")
-    ax.set_ylabel(f"{y_label} ({y_unit})")
+    ax.set_ylabel(f"{y_label} {y_unit}")
     ax.set_xlabel("Time (days)")
-    fig.savefig(os.path.join(path, title.replace(" ", "")))
+    # fig.savefig(os.path.join(path, title.replace(" ", "")))
 
 
 if __name__ == "__main__":
@@ -31,16 +32,22 @@ if __name__ == "__main__":
     data = pd.read_csv(data_file)
     x = data["Time"]
     hyphal_sum = data["Active Hyphal"] + data["Tip"]
+    plt.rcParams.update({'font.size': 22})
+    fig1, ax1 = plt.subplots(figsize=(20, 14), nrows=2, ncols=2, constrained_layout=True)
+    fig2, ax2 = plt.subplots(figsize=(20, 14), nrows=2, ncols=2, constrained_layout=True)
 
-    create_plot(x, data["Tip"], "Tip", path, "Tips Cells", "scatter")
-    create_plot(x, data["Active Hyphal"], "Active Hyphal", path, "Active Cells")
-    create_plot(x, data["External Nutrition"], "External Nutrition", path, "External Nutrition", "mol")
-    create_plot(x, data["Internal Nutrition"], "Internal Nutrition", path, "Internal Nutrition", "mol")
-    create_plot(x, data["Internal Nutrition"]/hyphal_sum, "Average internal nutrition", path,
-                "Average internal nutrition", "mol")
+    create_plot(x, data["Tip"], "Tip",  "Tips Cells", ax1[0][0])
+    create_plot(x, data["Active Hyphal"], "Active Hyphal", "Active Cells", ax1[0][1])
+    create_plot(x, hyphal_sum, "Hyphal Cells", "Hyphal Cells", ax1[1][0])
+    create_plot(x, hyphal_sum / data['Tip'], "HGU", "HGU", ax1[1][1], "μm")
 
-    create_plot(x, hyphal_sum, "Hyphal Cells", path, "Hyphal Cells")
+    create_plot(x, data["External Nutrition"], "External Nutrition", "External Nutrition", ax2[0][0], "mol")
+    create_plot(x, data["Internal Nutrition"], "Internal Nutrition", "Internal Nutrition", ax2[0][1], "mol")
+    create_plot(x, data["Internal Nutrition"]/hyphal_sum, "Average Internal Nutrition",
+                "Average internal nutrition", ax2[1][0], "mol")
+
     create_plot(x[:-1], [hyphal_sum[i+1] - hyphal_sum[i] for i in range(len(hyphal_sum) - 1)],
-                "Hyphal Difference", path, "Hyphal Difference", "scatter")
+                "Hyphal Difference", "Hyphal Difference", ax2[1][1], plot_type="scatter")
 
-    create_plot(x, hyphal_sum / data['Tip'], "HGU", path, "HGU", "μm")
+    fig1.savefig(os.path.join(path, "fig1"))
+    fig2.savefig(os.path.join(path, "fig2"))
